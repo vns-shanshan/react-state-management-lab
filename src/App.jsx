2caa82a604a463edf9d13ea54caf2b1c5be72f44
huijingj <vanessajin0426@gmail.com>
2024-09-24 12:21:15 -0700

    Rendered fighters info

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,128 @@
 // src/App.jsx
+import { useState } from "react";
+import "./App.css";
 
+// ------- Initial States ----------
+const team = [];
+const money = 100;
+const zombieFighters = [
+  {
+    name: "Survivor",
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: "https://via.placeholder.com/150/92c952",
+  },
+  {
+    name: "Scavenger",
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: "https://via.placeholder.com/150/771796",
+  },
+  {
+    name: "Shadow",
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: "https://via.placeholder.com/150/24f355",
+  },
+  {
+    name: "Tracker",
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: "https://via.placeholder.com/150/d32776",
+  },
+  {
+    name: "Sharpshooter",
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: "https://via.placeholder.com/150/1ee8a4",
+  },
+  {
+    name: "Medic",
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: "https://via.placeholder.com/150/66b7d2",
+  },
+  {
+    name: "Engineer",
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: "https://via.placeholder.com/150/56acb2",
+  },
+  {
+    name: "Brawler",
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: "https://via.placeholder.com/150/8985dc",
+  },
+  {
+    name: "Infiltrator",
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: "https://via.placeholder.com/150/392537",
+  },
+  {
+    name: "Leader",
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: "https://via.placeholder.com/150/602b9e",
+  },
+];
+// -----------------------------------
+
+export function ZombieFighter({ zombieFighter }) {
+  const { name, price, strength, agility, img } = zombieFighter;
+
+  return (
+    <li>
+      <img src={img} alt={name} />
+
+      <p>{name}</p>
+      <p>Price: {price}</p>
+      <p>Strength: {strength}</p>
+      <p>Agility: {agility}</p>
+
+      <button onClick={handleAddFighter}>Add</button>
+    </li>
+  );
+}
+
+function handleAddFighter() {
+  console.log("Fighter Added!");
+}
+
+// ----------- App -----------------
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [team, setTeam] = useState([]);
+
+  return (
+    <>
+      <h1>Zombie Fighters</h1>
+
+      <h2>Money: {money}</h2>
+      <h2>Team Strength: </h2>
+      <h2>Team Agility: </h2>
+      <h2>Team</h2>
+      <ul>{team}</ul>
+
+      <h2>Fighters</h2>
+      <ul>
+        {zombieFighters.map((zombieFighter, index) => (
+          <ZombieFighter zombieFighter={zombieFighter} key={index} />
+        ))}
+      </ul>
+    </>
+  );
 };
+// -----------------------------------
 
 export default App;
